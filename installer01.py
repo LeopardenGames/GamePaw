c0116a32f1de887da36cb37239d81c03bb53f7d1
LeopardenGames <119418555+LeopardenGames@users.noreply.github.com>
2024-09-19 19:14:53 +0200

    Update installer01.py

--- installer01.py
+++ installer01.py
@@ -1,18 +1,43 @@
-def ask_questions():
-    answers = {}
-    
-    # Fragen an den Benutzer stellen
-    answers['Username'] = input("Set Username")
-    answers['Passwort'] = input("Set Passwort")
-    
-    return answers
+import getpass
+import os
+import json
+from colorama import init, Fore, Style
 
-def write_conf_file(filename, answers):
+# Initialisiere colorama
+init()
+
+def prompt_user_for_info():
+    """Fragt den Benutzer nach seinen Registrierungsdaten und gibt ein Dictionary zurück."""
+    print(Fore.CYAN + "Willkommen zur Benutzerregistrierung!" + Style.RESET_ALL)
+    
+    username = input(Fore.GREEN + "Benutzername: " + Style.RESET_ALL)
+    
+    # Passwort eingeben
+    password = getpass.getpass(Fore.GREEN + "Passwort: " + Style.RESET_ALL)
+    confirm_password = getpass.getpass(Fore.GREEN + "Passwort bestätigen: " + Style.RESET_ALL)
+    
+    if password != confirm_password:
+        print(Fore.RED + "Die Passwörter stimmen nicht überein. Versuche es erneut." + Style.RESET_ALL)
+        return None
+    
+    email = input(Fore.GREEN + "E-Mail-Adresse: " + Style.RESET_ALL)
+    
+    return {
+        'username': username,
+        'password': password,
+        'email': email
+    }
+
+def save_to_conf_file(data, filename='user_info.conf'):
+    """Speichert die Benutzerdaten in einer .conf-Datei im JSON-Format."""
     with open(filename, 'w') as f:
-        for key, value in answers.items():
-            f.write(f"{key} = {value}\n")
+        json.dump(data, f, indent=4)
+    print(Fore.BLUE + f"Die Daten wurden in '{filename}' gespeichert." + Style.RESET_ALL)
+
+def main():
+    user_data = prompt_user_for_info()
+    if user_data:
+        save_to_conf_file(user_data)
 
 if __name__ == "__main__":
-    user_answers = ask_questions()
-    write_conf_file('config/user_info.conf', user_answers)
-    print("Die Antworten wurden in 'user_info.conf' gespeichert.")
+    main()
